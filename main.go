91e6b09d6d88647884385dd928301707cd22b691
Gustavo <gustavosantossilveira12360@gmail.com>
2023-07-24 12:25:58 -0300

    Big O

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	sumCharCods("gustavo")
+}
+
+//Testando o big O(custo computacional de uma função)
+func sumCharCods(n string) int {
+	nRune := []rune(n)
+	var sum int
+	for i := 0; i < len(n); i++ {
+		for j := 0; j < len(n); j++ {
+			for k := 0; k < len(n); k++ {
+				for l := 0; l < len(n); l++ {
+					for m := 0; m < len(n); m++ {
+						for o := 0; o < len(n); o++ {
+							sum += int(nRune[o])
+							fmt.Println(sum)
+						}
+					}
+				}
+			}
+		}
+	}
+	return sum
+}
